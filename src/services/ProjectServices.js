c2f5ee76d954d8ab6eb6e4dde43bd25ed6de7f29
Young-Einstein10 <aabdulrahmanyusuf@gmail.com>
2022-05-03 12:54:27 +0100

    fix proxy url

--- src/services/ProjectServices.js
+++ src/services/ProjectServices.js
@@ -1,32 +1,60 @@
 import { req } from "./httpService";
 
+const API_SERVICE_URL =
+  "http://budgitapi-env.eba-82vtvuzm.eu-west-2.elasticbeanstalk.com";
+const isProd = process.env.NODE_ENV === "production";
+
 const ProjectServices = {
   getFederalAllocations() {
-    return req.get("/budgitapi/index.php/federals");
+    return req.get(
+      isProd
+        ? API_SERVICE_URL + "/index.php/federals"
+        : "/budgitapi/index.php/federals"
+    );
   },
 
   getStateAllocations() {
-    return req.get("/budgitapi/index.php/state-allocations");
+    return req.get(
+      isProd
+        ? API_SERVICE_URL + "/index.php/state-allocations"
+        : "/budgitapi/index.php/state-allocations"
+    );
   },
 
   getLgaAllocations(lga, state, pageNumber = 1, postPerPage = 10) {
-    return req.get(`/budgitapi/index.php/lga-allocations?lga=${lga}&state=${state}&page=${pageNumber}&count=${postPerPage}
-    `);
+    return req.get(
+      isProd
+        ? API_SERVICE_URL +
+            `/index.php/lga-allocations?lga=${lga}&state=${state}&page=${pageNumber}&count=${postPerPage}
+        `
+        : `/budgitapi/index.php/lga-allocations?lga=${lga}&state=${state}&page=${pageNumber}&count=${postPerPage}
+    `
+    );
   },
 
   getAllocationsPerPage(pageNumber = 1, postPerPage = 10) {
     return req.get(
-      `/budgitapi/index.php/state-allocations?page=${pageNumber}&count=${postPerPage}`
+      isProd
+        ? API_SERVICE_URL +
+            `/index.php/state-allocations?page=${pageNumber}&count=${postPerPage}`
+        : `/budgitapi/index.php/state-allocations?page=${pageNumber}&count=${postPerPage}`
     );
   },
 
   getProjects() {
-    return req.get("/budgitapi/index.php/projects");
+    return req.get(
+      isProd
+        ? API_SERVICE_URL + "/index.php/projects"
+        : "/budgitapi/index.php/projects"
+    );
   },
 
   getProjectsPerPage(pageNumber = 1, postPerPage = 10) {
     return req.get(
-      `/budgitapi/index.php/projects?page=${pageNumber}&count=${postPerPage}`
+      isProd
+        ? API_SERVICE_URL +
+            `/index.php/projects?page=${pageNumber}&count=${postPerPage}`
+        : `/budgitapi/index.php/projects?page=${pageNumber}&count=${postPerPage}`
     );
   },
 
